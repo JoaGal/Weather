fd5cff51901bd99c3a94755e44b1084222578a72
JoaGal <joaquingaldeano7@gmail.com>
2023-04-21 14:40:42 -0300

    Last

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,6 +90,7 @@ export default function Home() {
   // };
   return (
       <main>
+        
         <div className="container">
           <div className="current-info">
             <div className="date-container">
