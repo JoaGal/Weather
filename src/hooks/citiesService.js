683b4d6338b42c6ea9a50ae0f0e002ae70f3b350
JoaGal <joaquingaldeano7@gmail.com>
2023-05-03 21:31:32 -0300

    Last Commit

--- src/hooks/citiesService.js
+++ src/hooks/citiesService.js
@@ -1,7 +1,8 @@
 export const citiesService = async (city, setWeather) => {
+  const keyURL = "6bf11c60285b9b4e9ef3802f78058417"
   try {
       const res = await fetch(
-        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${keyURL}`
       );
       const data = await res.json();
       setWeather(data);
