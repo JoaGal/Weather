f3807628109f11dcfb7428383924a4c83cd5cc3b
JoaGal <joaquingaldeano7@gmail.com>
2023-05-03 21:09:37 -0300

    Last Commit

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -11,7 +11,7 @@ import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAObuYHNF9jL1S9NygPTzww9d-WOQSTmqY",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
   authDomain: process.env.NEXT_PUBLIC_AUTH,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_BUCKET,
